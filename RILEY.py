57f17ad5e05ce5524aa2ca8b399a9df0f35584ed
purpl <decker_rossi@live.com>
2022-03-07 17:29:46 -0800

    RILEY FunctionFile interface

--- RILEY.py
+++ RILEY.py
@@ -1,5 +1,9 @@
 #the 'thinking' table: logic , meaning and 'emotional' functions utilising specific definitions and comparing them to
 #an sql table
-#also the location of personality - intended to be snarky and overly sarcastic and occasionaly rude but compliant and not malicious in the slightest.
-def EmotionalComprehension():
-    pass
+#also the location of personality - intended to be snarky and overly sarcastic and occasionaly rude but compliant and not malicious in the slightest. Gender: NB, pronouns they/them (Not too important with one-on-one communication but should development expand to the point where Riley is meeting new people then they ought to be incorporated.
+#Personality class to be the summation - inputs are to be fed through the personality and learning classes. Output dictated by input (so it would be appropriate)
+#and fed through the Personality class so it aligns with the way Riley is supposed to be.
+class Personality():
+
+
+class Learning():
