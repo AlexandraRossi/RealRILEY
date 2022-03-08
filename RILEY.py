f3726ce0cf1ba808bc22a33bbb76f4a8302add7e
purpl <decker_rossi@live.com>
2022-03-07 17:23:56 -0800

    RILEY FunctionFile interface

--- RILEY.py
+++ RILEY.py
@@ -1,4 +1,5 @@
 #the 'thinking' table: logic , meaning and 'emotional' functions utilising specific definitions and comparing them to
-#an sql table 
+#an sql table
+#also the location of personality - intended to be snarky and overly sarcastic and occasionaly rude but compliant and not malicious in the slightest.
 def EmotionalComprehension():
     pass
